e09af02e2e41c7fb2a8ad2add2d28eff3bc9277a
Charlize de Beer <charlizedb@aie.ac>
2026-03-27 03:24:29 +0200

    fix: bare module.exports handler for login

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,15 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
-function handler(_req, res) {
-    const clientId = process.env.GITHUB_CLIENT_ID;
-    if (!clientId) {
-        return res.status(500).json({ error: 'GITHUB_CLIENT_ID not configured' });
-    }
-    const redirectUri = `${process.env.VITE_APP_URL || 'http://localhost:3000'}/api/auth/callback`;
-    const scope = 'read:user repo';
-    const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
-    res.redirect(302, url);
-}
-
-if (typeof exports.default !== 'undefined') { module.exports = exports.default; }
+module.exports = function handler(req, res) {
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  if (!clientId) {
+    return res.status(500).json({ error: 'GITHUB_CLIENT_ID not configured' });
+  }
+  const redirectUri = (process.env.VITE_APP_URL || 'http://localhost:3000') + '/api/auth/callback';
+  const scope = 'read:user repo';
+  const url = 'https://github.com/login/oauth/authorize?client_id=' + clientId + '&redirect_uri=' + encodeURIComponent(redirectUri) + '&scope=' + encodeURIComponent(scope);
+  res.redirect(302, url);
+};
